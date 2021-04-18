19dc17c5fa4e5f7ac41c8a4c43b520c295099dd6
zyb <zyb990608@gmail.com>
2021-04-18 22:56:59 +0800

    add all files

---
+++ a/sspexp_codes/example.sh
@@ -0,0 +1,3 @@
+./bin/sspexp_run -x -w 1 -s 0 -m 0 -g graph_example -e label_example_PLL
+./bin/sspexp_run -x -w 1 -s 0 -m 1 -g graph_example -e label_example_RXL
+./bin/sspexp_run -x -w 1 -s 0 -m 2 -g graph_example -e label_example_SHP
