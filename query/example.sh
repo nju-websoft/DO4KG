00e848e23b86c8f598831caa09c7b9f862d5b6b3
yangbo.zhang <yangbo.zhang@tusimple.ai>
2021-04-19 19:48:18 +0800

    a

--- query/example.sh
+++ query/example.sh
@@ -1,7 +1,7 @@
 ./query 0 query_example.in ../SHL/rank_example ../SHL/label_example SHL_query_example.out
-./query 1 query_example.in ../a/sspexp_codes/label_example_PLL.label PLL_query_example.out
-./query 1 query_example.in ../a/sspexp_codes/label_example_RXL.label RXL_query_example.out
-./query 1 query_example.in ../a/sspexp_codes/label_example_SHP.label SHP_query_example.out
+./query 1 query_example.in ../PLL-RXL-SHP/sspexp_codes/label_example_PLL.label PLL_query_example.out
+./query 1 query_example.in ../PLL-RXL-SHP/sspexp_codes/label_example_RXL.label RXL_query_example.out
+./query 1 query_example.in ../PLL-RXL-SHP/sspexp_codes/label_example_SHP.label SHP_query_example.out
 ./query 2 query_example.in 10 ../HL/graph_example ../HL/label_example HL_query_example.out
 ./query 3 query_example.in ../H2H/label_example H2H_query_example.out
 ./query 4 query_example.in ../PHL/label_example PHL_query_example.out
